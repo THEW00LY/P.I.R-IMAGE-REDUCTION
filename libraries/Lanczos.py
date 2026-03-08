3609b6dc20a4b74a4dc74c2adaa230a72fa716b7
mathieuvaccaro <vaccaro@insa-toulouse.fr>
2026-03-08 22:53:26 +0100

    Version 1.0 with C langage library. it works much more faster, but it's not as perfect as PIL version

---
+++ libraries/Lanczos.py
@@ -0,0 +1,29 @@
+import ctypes
+import os
+import sys
+import numpy as np
+
+# --- Chargement automatique de la lib selon l'OS ---
+def _charger_lib():
+    dossier = os.path.dirname(os.path.abspath(__file__))
+    if sys.platform == "win32":
+        nom = "lanczos.dll"
+    elif sys.platform == "darwin":
+        nom = "malib.dylib"
+    else:
+        nom = "malib.so"
+    
+    chemin = os.path.join(dossier, nom)
+    return ctypes.CDLL(chemin)
+
+_lib = _charger_lib()
+
+
+# argtypes -> Paramètres
+# restypes -> retours
+_lib.lanczos.argtypes = [ctypes.c_void_p, ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.c_int]
+_lib.lanczos.restype  = ctypes.c_void_p
+
+# --- API publique : fonctions Python normales ---
+def lanczos(img, old_h, old_w, new_h, new_w, a):
+    return _lib.lanczos(img, old_h, old_w, new_h, new_w, a)
